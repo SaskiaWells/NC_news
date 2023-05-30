d24d9fa494dbf926e86a5307e98773ed398474db
SaskiaWells <sassywbernie@gmail.com>
2023-05-30 18:06:01 +0100

    small fix

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-require('dotenv').config({
+
+
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+console.log("ENV:", ENV);
+
+console.log(process.env.DATABASE_URL);
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set or DATABASE_URL not set");
